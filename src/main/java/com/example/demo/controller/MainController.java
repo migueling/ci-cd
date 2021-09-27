82ae5e475cb46f703fd1a3f6fdab50ee4083c8f0
Miguel Angel Sanchez Solis <ing.miguels@gmail.com>
2021-09-26 19:40:40 -0500

    Added java version

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @GetMapping("/")
-    public String getUsers() {
-        return "Hola";
+    public String hello() {
+        return "Hello world";
     }
 }
