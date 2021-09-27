85443e261e6cc43dfcadf1921ce027db4cccaede
Miguel Angel Sanchez Solis <ing.miguels@gmail.com>
2021-09-26 20:50:58 -0500

    Added test

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @GetMapping("/")
-    public String hello() {
+    public String helloWorld() {
         return "Hello world";
     }
 }
